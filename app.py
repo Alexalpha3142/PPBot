8d8b84471f97f06be3cdb1ddbd541ab6b65a01d0
Alexalpha3142 <94995347+Alexalpha3142@users.noreply.github.com>
2026-04-05 16:49:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def home():
     return "I am alive!"
 
 def run():
-port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port) # Строго 0.0.0.0
 
 def keep_alive():
